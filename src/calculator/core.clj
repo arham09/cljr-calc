bd5408f3f0f1289672476e4013bad70533c9bb07
Arham Abiyan <arham.abiyan@gmail.com>
2020-11-03 10:40:30 +0700

    using ns to separate func

--- src/calculator/core.clj
+++ src/calculator/core.clj
@@ -1,41 +1,7 @@
 (ns calculator.core
+  (:require [calculator.calculator :as calculator])
   (:gen-class))
 
-(defn sum
-  "sum 2 numbers"
-  [a b]
-  (+ a b))
-
-(defn subtract
-  "subtract 2 numbers"
-  [a b]
-  (- a b))
-
-(defn multiply
-  "multiply 2 numbers"
-  [a b]
-  (* a b))
-
-(defn divide
-  "divide 2 numbers"
-  [a b]
-  (double (/ a b)))
-
-(defn parse-int
-  "parse string to integer"
-  [s]
-  (Integer. (re-find  #"\d+" s )))
-
-(defn calculate
-  "Calculate based on input"
-  [choice]
-  (println "Insert Two Numbers")
-  (let [a (read-line) b (read-line)]
-    (cond
-      (= choice "sum") (println "Result is" (sum (parse-int a) (parse-int b)))
-      (= choice "subtraction") (println "Result is" (subtract (parse-int a) (parse-int b)))
-      (= choice "multiplication") (println "Result is" (multiply (parse-int a) (parse-int b)))
-      (= choice "division") (println "Result is" (divide (parse-int a) (parse-int b))))))
 
 (defn -main
   "Main calculator func"
@@ -43,4 +9,4 @@
   (println "1. sum \n2. division \n3. multiplication \n4. subtraction")
   (println "Enter number fo your choice")
   (let [choice (read-line)]
-    (calculate choice)))
+    (calculator/calculate choice)))

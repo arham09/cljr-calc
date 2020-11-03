37fdb1d5356257139720b59eaf639d214e72b7b6
Arham Abiyan <arham.abiyan@gmail.com>
2020-11-03 11:44:55 +0700

    add unit test for calculator function

--- test/calculator/core_test.clj
+++ test/calculator/core_test.clj
@@ -7,3 +7,15 @@
   (testing "Sum Function of calculator"
     (is (= 6 (calculator/sum 3 3)))))
 
+(deftest subtract-test
+  (testing "Subtract function of calculator"
+    (is (= 2 (calculator/subtract 10 8)))))
+
+(deftest multiplication-test
+  (testing "Multiplication function of calculator"
+    (is (= 10 (calculator/multiply 5 2)))))
+
+(deftest division-test
+  (testing "Division function of calculator"
+    (is (= (double 5) (calculator/divide 10 2)))))
+
